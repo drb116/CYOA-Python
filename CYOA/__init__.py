efec4c465bb2a309e199ca35957c2f6a41908435
David Burnham <32823515+drb116@users.noreply.github.com>
2019-01-19 22:09:22 -0500

    Added more files

---
+++ CYOA/__init__.py
@@ -0,0 +1,11 @@
+print("Welcome to my Choose your own adventure!")
+
+def stay_in_bed():
+    print("You stayed in bed and call for your mom to get you breakfast.")
+    
+
+if (input("You wake up. Do you stay in BED or GET UP?") == "bed"):
+    stay_in_bed()
+else:
+    print("You got up")
+  
